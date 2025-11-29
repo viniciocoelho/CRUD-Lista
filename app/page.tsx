b30e62a04015a2aa73176ed0f7d10ae9a6addeb5
viniciocoelho <coelhocvcg@gmail.com>
2025-11-29 00:47:27 -0300

    CRUD React
    Finalização do CRUD

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,79 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { useState } from "react";
+import { Todoitem } from "./types/Todoitem";
+
+const Page = () => {
+
+  const[itemInput, setitemInput] = useState('');
+
+  const[list, setList] = useState<Todoitem[]>([
+    { label: 'Fazer dever de casa', checked: true },
+    { label: 'Comprar o bolo', checked: false }
+  ]);
+
+  const handleAddButton = () => {
+    if(itemInput.trim() === '') return;
+
+    setList([
+      ...list,
+      { label: itemInput, checked: false}
+    ]);
+    setitemInput('');
+  }
+
+  const deleteItem = (index: number) => {
+    setList(list.filter((item, key) => key !== index));
+
+  }
+
+  const tooggleItem = (index: number) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(index === parseInt(i)) {
+        newList[i].checked = !newList[i].checked;
+
+      }
+      setList(newList);
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl"> 
+    <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
+
+    <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-500">
+      <input 
+        type="text" 
+        placeholder="O que deseja fazer?"
+        className="flex-1 border border-white p-3 text-2xl text-white rouded-md mr-3"
+        value={itemInput}
+        onChange={e => setitemInput(e.target.value)}
+      />
+      <button onClick={handleAddButton}>Adicionar</button>
+
+    </div>
+
+    <p className="my-4">{list.length} itens na lista</p>
+
+    <ul>
+      {list.map((item, index) =>
+        <li key={index}>
+        <input onClick={() => tooggleItem(index)} type="checkbox" checked={item.checked} className="w-6 h-6 mr-3"/>
+        {item.label} - <button onClick={() => deleteItem(index)} className="hover:underline">[ deletar ]</button></li>
+      )}
+
+    </ul>
+      
+
+      
+ 
+ 
+
+   
+
     </div>
   );
 }
+
+export default Page;
